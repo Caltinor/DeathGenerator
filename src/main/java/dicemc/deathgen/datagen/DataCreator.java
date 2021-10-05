8ed77a96c5ebb59eb621ffccd11458caf062e931
MelanX <info@melanx.de>
2021-10-05 13:17:18 +0200

    add data generation for item models, block states and tags

---
+++ src/main/java/dicemc/deathgen/datagen/DataCreator.java
@@ -0,0 +1,26 @@
+package dicemc.deathgen.datagen;
+
+import dicemc.deathgen.DeathGenerator;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
+
+@Mod.EventBusSubscriber(modid = DeathGenerator.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DataCreator {
+
+    @SubscribeEvent
+    public static void onGatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        ExistingFileHelper helper = event.getExistingFileHelper();
+
+        if (event.includeServer()) {
+            generator.addProvider(new EntityTagsProvider(generator, helper));
+        }
+        if (event.includeClient()) {
+            generator.addProvider(new ItemModelProvider(generator, helper));
+            generator.addProvider(new BlockStateProvider(generator, helper));
+        }
+    }
+}
---
+++ src/main/java/dicemc/deathgen/datagen/ItemModelProvider.java
@@ -0,0 +1,57 @@
+package dicemc.deathgen.datagen;
+
+import dicemc.deathgen.DeathGenerator;
+import dicemc.deathgen.setup.Registration;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.client.model.generators.ModelFile;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.fmllegacy.RegistryObject;
+
+public class ItemModelProvider extends net.minecraftforge.client.model.generators.ItemModelProvider {
+
+    public ItemModelProvider(DataGenerator generator, ExistingFileHelper helper) {
+        super(generator, DeathGenerator.MOD_ID, helper);
+    }
+
+    @Override
+    protected void registerModels() {
+        for (RegistryObject<Item> entry : Registration.ITEMS.getEntries()) {
+            Item item = entry.get();
+            if (item instanceof BlockItem blockItem) {
+                this.blockItem(blockItem);
+            } else {
+                this.generated(item);
+            }
+        }
+    }
+
+    private void blockItem(BlockItem item) {
+        ResourceLocation location = item.getRegistryName();
+        //noinspection ConstantConditions
+        this.getBuilder(location.getPath())
+                .parent(new AlwaysExistentModelFile(new ResourceLocation(location.getNamespace(), "block/" + location.getPath())));
+    }
+
+    private void generated(Item item) {
+        //noinspection ConstantConditions
+        String path = item.getRegistryName().getPath();
+        this.getBuilder(path)
+                .parent(this.getExistingFile(this.mcLoc("item/generated")))
+                .texture("layer0", "item/" + path);
+    }
+
+    private static class AlwaysExistentModelFile extends ModelFile {
+
+        protected AlwaysExistentModelFile(ResourceLocation location) {
+            super(location);
+        }
+
+        @Override
+        protected boolean exists() {
+            return true;
+        }
+    }
+}
